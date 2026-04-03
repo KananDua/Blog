c6f63979bf98f37320ea0fc88cb6d0639f7dcb60
Kanan <kanandua29@gmail.com>
2026-04-03 17:54:45 +0530

    Fix STATIC_ROOT for Render deploy

--- blogProject/settings.py
+++ blogProject/settings.py
@@ -102,6 +102,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 CRISPY_ALLOWED_TEMPLATE_PACKS = ['bootstrap4']
