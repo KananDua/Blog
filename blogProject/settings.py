c17b697acc2f28efdd9975b924a7d2604c6894bd
Kanan <kanandua29@gmail.com>
2026-04-03 11:11:44 +0530

    Added PostgreSQL config

--- blogProject/settings.py
+++ blogProject/settings.py
@@ -8,6 +8,9 @@ import os
 
 from pathlib import Path
 
+import dj_database_url
+import os
+
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,10 +75,9 @@ WSGI_APPLICATION = 'blogProject.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
