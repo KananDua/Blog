36e2d4023ea346fbfebd7e1f54b88179d458f78a
Kanan <kanandua29@gmail.com>
2026-04-03 10:34:32 +0530

    3.0

--- blogProject/settings.py
+++ blogProject/settings.py
@@ -28,10 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'crispy_forms',
     'crispy_bootstrap5',
-
     'blog',
     'users',
 ]
@@ -39,7 +37,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddlleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
